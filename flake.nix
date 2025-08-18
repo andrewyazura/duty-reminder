d92057ff06ebfc3a408241a73f449bd0c15b2af9
Andrew Yatsura <andrewyazura203@proton.me>
2025-08-18 05:11:50 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "telegram bot for reminding roommates about cleaning duties";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      lastModifiedDate =
+        self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          duty-reminder = pkgs.buildGoModule {
+            pname = "duty-reminder";
+            inherit version;
+            src = ./.;
+
+            # This hash locks the dependencies of this package. It is
+            # necessary because of how Go requires network access to resolve
+            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+            # details. Normally one can build with a fake hash and rely on native Go
+            # mechanisms to tell you what the hash should be or determine what
+            # it should be "out-of-band" with other tooling (eg. gomod2nix).
+            # To begin with it is recommended to set this, but one must
+            # remember to bump this hash when your dependencies change.
+            # vendorHash = pkgs.lib.fakeHash;
+
+            vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go gopls gotools go-tools ];
+          };
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.go-hello);
+    };
+}
