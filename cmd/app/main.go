75ca13e3e135d373008570af1f411eb9ad9ea97e
Andrew Yatsura <andrewyazura203@proton.me>
2025-08-24 13:44:24 +0200

    use uow and add telegram service to the event bus

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 	"os"
@@ -8,6 +9,8 @@ import (
 	"github.com/andrewyazura/duty-reminder/internal/config"
 	"github.com/andrewyazura/duty-reminder/internal/eventbus"
 	"github.com/andrewyazura/duty-reminder/internal/routes"
+	"github.com/andrewyazura/duty-reminder/internal/services"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
@@ -18,7 +21,17 @@ func main() {
 		logger.Error("couldn't build config", "error", err)
 	}
 
+	pool, err := pgxpool.New(context.Background(), config.Database.URL)
+	if err != nil {
+		logger.Error("couldn't start a db connection pool", "error", err)
+	}
+
+	uow := services.NewPostgresUnitOfWork(pool)
 	eventBus := eventbus.NewEventBus()
+
+	telegramService := services.NewTelegramService(eventBus, &config.Telegram, uow)
+	eventBus.Subscribe("TelegramUpdate", telegramService.HandleUpdate)
+
 	server := routes.NewServer(config.Server, config.Telegram, logger, eventBus)
 
 	logger.Info("starting server on port 8080")
