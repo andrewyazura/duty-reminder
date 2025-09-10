688ea3e6023620fe945ddade971ad031163735ed
Andrew Yatsura <andrewyazura203@proton.me>
2025-09-11 01:50:56 +0200

    WIP: rewrite flake

--- flake.nix
+++ flake.nix
@@ -3,34 +3,68 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }:
     let
-      lastModifiedDate =
-        self.lastModifiedDate or self.lastModified or "19700101";
-      version = builtins.substring 0 8 lastModifiedDate;
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      duty-reminder = pkgs.buildGoModule {
+        pname = "duty-reminder";
+        version = self.shortRev or "dev";
+        src = ./.;
+        vendorHash = "sha256-Qk4HXQ4RT7Glsrb/uo2iZEJj8c7SCnsBTY2a0LmD+vw=";
+
+        CGO_ENABLED = 0;
+        ldflags = [ "-s" "-w" ];
+        meta.mainProgram = "duty-reminder";
+      };
     in {
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          duty-reminder = pkgs.buildGoModule {
-            pname = "duty-reminder";
-            inherit version;
-            src = ./.;
-            vendorHash = "sha256-Qk4HXQ4RT7Glsrb/uo2iZEJj8c7SCnsBTY2a0LmD+vw=";
-          };
-        });
+      packages.${system}.default = duty-reminder;
+      packages.${system}.duty-reminder = duty-reminder;
 
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [ go gopls gotools go-tools postgresql ];
-          };
-        });
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ go gopls gotools go-tools postgresql ];
+      };
 
-      defaultPackage =
-        forAllSystems (system: self.packages.${system}.duty-reminder);
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/duty-reminder";
+      };
+
+      nixosModules.default = { lib, config, ... }:
+        let cfg = config.services.duty-reminder;
+        in {
+          options.services.duty-reminder = {
+            enable = lib.mkEnableOption "Enable duty-reminder service";
+
+            port = lib.mkOption { };
+          };
+
+          config = lib.mkIf cfg.enable {
+            users.users.duty-reminder = {
+              description = "Duty Reminder service user";
+              isSystemUser = true;
+              group = "duty-reminder";
+            };
+            users.groups.duty-reminder = { };
+
+            systemd.services.duty-reminder = {
+              description = "Duty Reminder service";
+              after = [ "network-online.target" ];
+              wants = [ "network-online.target" ];
+              wantedBy = [ "multi-user.target" ];
+
+              environment = cfg.environment;
+
+              serviceConfig = {
+                User = "duty-reminder";
+                Group = "duty-reminder";
+
+                ExecStart =
+                  "${self.packages.${system}.default}/bin/duty-reminder";
+
+                Type = "simple";
+                Restart = "on-failure";
+              };
+            };
+          };
+        };
     };
 }
